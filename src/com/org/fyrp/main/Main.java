8681779b21397ae419783e9a163bb6f9f46fd0aa
froggerman330 <froggerman330@gmail.com>
2015-10-06 21:47:49 +0000

    Main file and test file for unit tests.

---
+++ src/com/org/fyrp/main/Main.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.org.fyrp.main;
+
+import org.openkinect.freenect.*;
+
+
+/**
+ * @author pi
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Context freenectContext = Freenect.createContext();
+		
+
+	}
+
+}
